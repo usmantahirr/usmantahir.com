0931f1d612c674aec6631a3b0d4b77da47f2cc8d
Usman Tahir <usman.tahir@dpdk.com>
2023-10-05 11:54:36 +0200

    Added top section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,19 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Header from "@/app/components/header";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Header from '@/app/components/header';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Usman Tahir | Portfolio",
-  description: "Usman is a Full Stack Developer with 10+ years of experience",
+  title: 'Usman Tahir | Portfolio',
+  description: 'Usman is a Full Stack Developer with 10+ years of experience',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
         <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
         <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Intro from '@/app/components/intro';
+import SectionDivider from '@/app/components/section-divider';
+import About from '@/app/components/about';
+
 export default function Home() {
-  return <main className="flex flex-col items-center px-4"></main>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
+    </main>
+  );
 }
