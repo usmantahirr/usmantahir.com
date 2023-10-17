b1b8752482e248b8d055625eba15de1c15ac0aa9
Usman Tahir <usman.tahir@dpdk.com>
2023-10-17 16:52:36 +0200

    active section on scroll function added

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -1,15 +1,25 @@
 'use client';
 
 import Image from 'next/image';
+import React from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithubSquare } from 'react-icons/fa';
+import { useSectionInView } from '@/lib/hooks';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Intro() {
+  const { ref } = useSectionInView('Home', 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
   return (
-    <section id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -22,7 +32,7 @@ export default function Intro() {
           >
             <Image
               src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Usman Portrait"
+              alt="Ricardo portrait"
               width="192"
               height="192"
               quality="95"
@@ -52,9 +62,9 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Usman.</span> I'm a{' '}
+        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{' '}
         <span className="font-bold">full-stack developer</span> with{' '}
-        <span className="font-bold">10 years</span> of experience. I enjoy building{' '}
+        <span className="font-bold">8 years</span> of experience. I enjoy building{' '}
         <span className="italic">sites & apps</span>. My focus is{' '}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
@@ -70,7 +80,10 @@ export default function Intro() {
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {}}
+          onClick={() => {
+            setActiveSection('Contact');
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here{' '}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,17 +1,22 @@
-import { Fragment } from 'react';
-import SectionHeading from '@/app/components/section-heading';
+'use client';
+
+import React from 'react';
+import SectionHeading from './section-heading';
 import { projectsData } from '@/lib/data';
 import Project from './project';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Projects() {
+  const { ref } = useSectionInView('Projects', 0.5);
+
   return (
-    <section id="projects" className="scroll-mt-28">
-      <SectionHeading>My Projects</SectionHeading>
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
-          <Fragment key={index}>
+          <React.Fragment key={index}>
             <Project {...project} />
-          </Fragment>
+          </React.Fragment>
         ))}
       </div>
     </section>
