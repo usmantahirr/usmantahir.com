e655bc9ed3dd40c4243225eef87c750bf57c70f3
Usman Tahir <usman.tahir@dpdk.com>
2023-10-19 14:37:30 +0200

    experience section fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Intro from '@/app/components/intro';
 import SectionDivider from '@/app/components/section-divider';
 import About from '@/app/components/about';
 import Projects from '@/app/components/projects';
+import Skills from '@/app/components/skills';
+import Experience from '@/app/components/experience';
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
+      <Skills />
+      <Experience />
     </main>
   );
 }
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -5,11 +5,14 @@ import type { SectionName } from './types';
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const { ref, inView } = useInView({
+    // I want it to be active when it's 75% in view. We use threshold = 0.75 for it. can be changed based on parameter
     threshold,
   });
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(() => {
+    // we want to disable the auto update when we click on a button, so it doesn't enable the pills in the middle while moving
+    // we check if time of the last click is more than a second ago
     if (inView && Date.now() - timeOfLastClick > 1000) {
       setActiveSection(sectionName);
     }
