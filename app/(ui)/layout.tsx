54c8b897fc9e3044c85b2ac909f4d73b25c442bc
Usman Tahir <hello@usmantahir.com>
2024-01-06 19:22:32 +0100

    Base sanity integration

--- app/(ui)/layout.tsx
+++ app/(ui)/layout.tsx
@@ -10,15 +10,11 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Usman | Personal Portfolio",
+  description: "Usman is a full-stack developer with 8 years of experience.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="!scroll-smooth">
       <body
